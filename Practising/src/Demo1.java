855a4387c46ccbdde7d923589223c06d9d1610d1
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-06-04 21:26:35 +0530

    commit 2 & conflits 1

--- Practising/src/Demo1.java
+++ Practising/src/Demo1.java
@@ -4,10 +4,10 @@ public class Demo1 {
 	public static void main(String[] args) {
 
 		System.out.println("Demo1 class");
-		int a =10;
-		int b= 20;
-		int c=a+b;
-		System.out.println(c);
+		int  x=10;
+		int y= 20;
+		int z=x+y;
+		System.out.println(z);
 	}
 
 }
