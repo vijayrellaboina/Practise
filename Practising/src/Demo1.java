0798a4a8cb5ee5ddd43e82210476c21a2a66e1dc
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-06-04 21:27:38 +0530

    commit 3 & conflit will occur

--- Practising/src/Demo1.java
+++ Practising/src/Demo1.java
@@ -4,8 +4,8 @@ public class Demo1 {
 	public static void main(String[] args) {
 
 		System.out.println("Demo1 class");
-		int a =10;
-		int b= 20;
+		int a =100;
+		int b= 200;
 		int c=a+b;
 		System.out.println(c);
 	}
