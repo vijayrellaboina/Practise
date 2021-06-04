3ab3c3642ea998c4d25112d606dc1f4fc736398b
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-06-04 21:21:19 +0530

    commit 1

---
+++ Practising/src/Demo1.java
@@ -0,0 +1,13 @@
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Demo1 class");
+		int a =10;
+		int b= 20;
+		int c=a+b;
+		System.out.println(c);
+	}
+
+}
